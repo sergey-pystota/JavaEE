f42eca5385af90cf67abfd5d0082d905e7d09c91
sergey-pystota <rockyroadtodublin161@gmail.com>
2020-12-31 12:56:55 +0300

    Last HW. Servlet

--- src/main/java/by/teachmeskills/servlet/BookShopServlet.java
+++ src/main/java/by/teachmeskills/servlet/BookShopServlet.java
@@ -2,10 +2,15 @@ package by.teachmeskills.servlet;
 
 import by.teachmeskills.DAOInitializer;
 import by.teachmeskills.dao.impl.mysql.MySQLDAOInitializer;
-import by.teachmeskills.module.book.BookEntity;
+import by.teachmeskills.dao.orm.init.OrmDAOInitializer;
+import by.teachmeskills.entity.adress.AddressEntity;
+import by.teachmeskills.entity.book.BookEntity;
+import by.teachmeskills.entity.client.ClientEntity;
+import by.teachmeskills.service.address.AddressService;
+import by.teachmeskills.service.address.AddressServiceImpl;
+import by.teachmeskills.service.client.ClientService;
+import by.teachmeskills.service.client.ClientServiceImpl;
 import by.teachmeskills.service.dto.BookDTO;
-import by.teachmeskills.service.dto.BookDTOService;
-import by.teachmeskills.service.impl.BookServiceDTO;
 import by.teachmeskills.service.impl.BooksServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -25,20 +30,28 @@ public class BookShopServlet extends HttpServlet {
 
     private static final Logger LOGGER = LogManager.getLogger(BookShopServlet.class);
     private static final DAOInitializer DAO_INITIALIZER = new MySQLDAOInitializer();
+    private static final DAOInitializer ORM_DAO_INITIALIZER = new OrmDAOInitializer();
 
-    private BookDTOService booksService = new BookServiceDTO();
+
+    private ClientService clientService = new ClientServiceImpl();
+    private AddressService addressService = new AddressServiceImpl();
 
     @Override
     public void init() throws ServletException {
         super.init();
-        DAO_INITIALIZER.init();
+//        DAO_INITIALIZER.init();
+        ORM_DAO_INITIALIZER.init();
         LOGGER.info("Bookshop servlet initialized");
     }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        List<BookDTO> userBooks = booksService.getAllBooks();
+        List<BookDTO> userBooks = BooksServiceImpl.getInstance().getAllBooks();
         List<BookEntity> booksForAdmin = BooksServiceImpl.getInstance().getAll();
+        final List<ClientEntity> clients = clientService.getAll();
+        final List<AddressEntity> address = addressService.getALL();
+        request.setAttribute("address", address);
+        request.setAttribute("clients", clients);
         request.setAttribute("userbooks", userBooks);
         request.setAttribute("books", booksForAdmin);
         request.getRequestDispatcher("/bookshop.jsp").forward(request, response);
