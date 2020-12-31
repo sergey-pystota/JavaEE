7b13932bce43fe3c6ecde7498601dbf5acd2d6fd
sergey-pystota <rockyroadtodublin161@gmail.com>
2020-12-31 13:02:54 +0300

    Last HW. Client and Address DAO interfaces

---
+++ src/main/java/by/teachmeskills/dao/AddressDAO.java
@@ -0,0 +1,6 @@
+package by.teachmeskills.dao;
+
+import by.teachmeskills.entity.adress.AddressEntity;
+
+public interface AddressDAO extends DAO<AddressEntity> {
+}
---
+++ src/main/java/by/teachmeskills/dao/client/ClientDAO.java
@@ -0,0 +1,7 @@
+package by.teachmeskills.dao.client;
+
+import by.teachmeskills.dao.DAO;
+import by.teachmeskills.entity.client.ClientEntity;
+
+public interface ClientDAO extends DAO<ClientEntity> {
+}
